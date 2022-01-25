6fe3504d056f2dcd4e2baecefa2dd1cd550ec52a
Hamish Henare <hamishjustin7@gmail.com>
2022-01-25 16:44:13 +1300

    added in a table

--- index.js
+++ index.js
@@ -2,37 +2,57 @@ const formSubmit = (e) => {
   e.preventDefault;
   let reps = document.getElementById("reps").value;
   let sets = document.getElementById("sets").value;
+  let weight = document.getElementById("weight").value;
 
-    let reps2ndTier = ((reps * sets)*2) / sets
-    let sets2ndTier = sets
-    let reps3rdTier = ((reps * sets)*3) / sets
-    let sets3rdTier = sets
+  document.getElementById("reps-2tier").innerHTML = `5`;
+  document.getElementById(
+    "sets-2tier"
+  ).innerHTML = `${calcSets2ndMovement(reps, sets, 2)}`;
+  document.getElementById(
+    "reps-3tier"
+  ).innerHTML = `${calcReps(reps, 3)}`;
+  document.getElementById(
+    "sets-3tier"
+  ).innerHTML = `${calcSets(reps, sets, 3)}`;
 
-    document.getElementById('reps-2tier').innerHTML = `Reps for 2nd movement = 5`
-    document.getElementById('sets-2tier').innerHTML = `Sets for 2nd movement = ${calcSets2ndMovement(reps, sets, 2)}`
-    document.getElementById('reps-3tier').innerHTML = `Reps for 3rd movement = ${calcReps(reps, 3)}`
-    document.getElementById('sets-3tier').innerHTML = `Sets for 3rd movement = ${calcSets(reps, sets, 3)}`
-    
-    document.getElementById('volume-2tier').innerHTML = `Total vol = ${vol(calcSets2ndMovement(reps, sets, 2), 5)}` 
-    document.getElementById('volume-3tier').innerHTML = `Total vol = ${vol(calcReps(reps, 3), calcSets(reps, sets, 3))}` 
+  document.getElementById(
+    "weight-2tier"
+  ).innerHTML = `${calcWeight(weight)}kg`;
+
+  document.getElementById("weight-3tier").innerHTML = `${
+    weight * 0.65
+  }kg`;
   return false;
 };
+
 const vol = (a, b) => {
-    return a*b
-}
+  return a * b;
+};
 
 const calcSets2ndMovement = (a, b, c) => {
-    let total = (vol(a, b) * c)
+  let total = vol(a, b) * c;
 
-    return Math.round(total / 5)
-}
+  return Math.round(total / 5);
+};
 
 const calcReps = (a, b) => {
-    return a*b
-}
+  return a * b;
+};
 
 const calcSets = (a, b, c) => {
-    let total = (vol(a,b)*c)
-    let reps =  a * c
-    return total / reps
-}
+  let total = vol(a, b) * c;
+  let reps = a * c;
+  return total / reps;
+};
+
+const calcWeight = (a) => {
+  if (a === 1) {
+    return a * 0.7;
+  } else if (a === 2) {
+    return a * 0.75;
+  } else if (a === 3) {
+    return a * 0.8;
+  } else if (a >= 4) {
+    return a * 0.85;
+  }
+};
