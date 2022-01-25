1fd9edc79e46722a2d20286c961ce0d63b42cc4d
Hamish Henare <hamishjustin7@gmail.com>
2022-01-25 14:45:40 +1300

    refactored but have to set a cap on reps per set for 2nd movement

--- index.js
+++ index.js
@@ -8,10 +8,25 @@ const formSubmit = (e) => {
     let reps3rdTier = ((reps * sets)*3) / sets
     let sets3rdTier = sets
 
-    document.getElementById('reps-2tier').innerHTML = `Reps for 2nd movement = ${reps2ndTier}`
-    document.getElementById('sets-2tier').innerHTML = `Sets for 2nd movement = ${sets2ndTier}`
-    document.getElementById('reps-3tier').innerHTML = `Reps for 3rd movement = ${reps3rdTier}`
-    document.getElementById('sets-3tier').innerHTML = `Sets for 3rd movement = ${sets3rdTier}`
-
+    document.getElementById('reps-2tier').innerHTML = `Reps for 2nd movement = ${calcReps(reps, 2)}`
+    document.getElementById('sets-2tier').innerHTML = `Sets for 2nd movement = ${calcSets(reps, sets, 2)}`
+    document.getElementById('reps-3tier').innerHTML = `Reps for 3rd movement = ${calcReps(reps, 3)}`
+    document.getElementById('sets-3tier').innerHTML = `Sets for 3rd movement = ${calcSets(reps, sets, 3)}`
+    
+    document.getElementById('volume-2tier').innerHTML = `Total vol = ${calcReps(reps, 2) * calcSets(reps, sets, 2)}` 
+    document.getElementById('volume-3tier').innerHTML = `Total vol = ${calcReps(reps, 3) * calcSets(reps, sets, 3)}` 
   return false;
 };
+const vol = (a, b) => {
+    return a*b
+}
+
+const calcSets = (a, b, c) => {
+    let total = (vol(a, b) * c)
+    let rep = a * c
+    return total / rep
+}
+
+const calcReps = (a, b) => {
+    return a*b
+}
